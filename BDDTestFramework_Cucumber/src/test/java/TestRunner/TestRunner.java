3719387dc1203b73604ab61175e8bf412121eb03
gp55551 <patnaikgaurav61@gmail.com>
2024-05-24 21:37:39 +0530

    new

---
+++ BDDTestFramework_Cucumber/src/test/java/TestRunner/TestRunner.java
@@ -0,0 +1,20 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions( dryRun = false,
+		features = "src\\Features\\FirstCucumber5.feature"	,
+		glue={"stepDefinition"},
+		monochrome=true
+		//,format = {"pretty"}
+		//,format = {"html:D:\\Results"}
+		
+	
+		)
+
+public class TestRunner {
+
+}
